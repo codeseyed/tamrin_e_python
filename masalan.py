d6f7da382a3ad5f34373ccbe643d3f60daa45cc5
masoud gholami <gongotar@masouds-MacBook-Pro.local>
2020-03-31 19:11:43 +0200

    read file, int type

--- masalan.py
+++ masalan.py
@@ -8,7 +8,11 @@ Created on Tue Mar 31 15:46:08 2020
 
 import numpy as np
 
-B = np.arange(0,10)
-print(B)
+B = np.arange(0,10, dtype=np.int)
+C = np.empty(10, dtype=np.int)
 
-np.savetxt('B.csv', B, delimiter=',', header='Please count these numbers!')
+
+np.savetxt('B.csv', B,fmt="%d", delimiter=',', header='Please count these numbers!')
+
+C = np.loadtxt('B.csv', delimiter =',')
+print(C)
