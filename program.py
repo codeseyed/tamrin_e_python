52d57130c227abea0b74027f2b4b2df55c3ec9c7
masoud gholami <gongotar@masouds-MacBook-Pro.local>
2020-03-31 19:17:42 +0200

    program comments

---
+++ program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 19:11:54 2020
+
+@author: gongotar
+"""
+
+# generate n * m 2D array with random numbers
+
+# store it in file
+
+# compute average of each cell with its neighbours until converge
+
+# load the first array from file
+
+# compute the difference between the first array and converged array
+
+# plot the converged array
