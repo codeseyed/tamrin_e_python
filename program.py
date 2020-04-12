03b29fa566ac0b42e7dd659856b4ac82b8d6c516
Mojtaba <seyedraoufi@gmail.com>
2020-04-12 17:55:40 +0200

    randomness was removed!

--- program.py
+++ program.py
@@ -8,15 +8,15 @@ Created on Tue Mar 31 19:11:54 2020
 import numpy as np
 import random as rnd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 #rnd.seed(1)
 
 def randomarray(n,m):
-    array = np.zeros((n,m))
-    for i in range(len(array)):
-        for j in range(len(array[i])):  
-            array[i][j] = rnd.choice(np.arange(0,20, dtype=np.int)) #assignign random number between 0 to 10 to each of matrix elements 
-    return array
+    h= 1
+    vector = np.arange(m*n, step=h, dtype=np.int)
+    random_matrix = np.reshape(vector, (n,m))
+    return random_matrix
 
 def neigh_average(random_matrix):
     averaged_matrix = np.zeros((len(random_matrix),len(random_matrix[0]))) #producing n*m zero matrix
@@ -56,8 +56,8 @@ def dffer(random_matrix, averaged_matrix):
 
 
 # generate n * m 2D array with random numbers
-n = 8
-m = 8
+n = 50
+m = 50
 random_matrix = randomarray(n, m)
 
 # store it in file
@@ -67,12 +67,12 @@ np.savetxt('random_matrix.csv', random_matrix, delimiter=',', fmt='%d')
 matrix_prev = random_matrix
 matrix_next = neigh_average(matrix_prev)
 differ = dffer(matrix_prev, matrix_next)
-while sum(sum(differ)) > n*m: #check whether convergance is achieved!
+while sum(sum(differ)) > m*n: #check whether convergance is achieved!
     matrix_prev = matrix_next
     matrix_next = neigh_average(matrix_prev)
     differ = dffer(matrix_prev, matrix_next)
-
-    
+print(sum(sum(differ)))
+ 
 print(matrix_next)           
     
 # load the first array from file
@@ -84,13 +84,19 @@ B = np.loadtxt('random_matrix.csv', delimiter=',')
 #differ = dffer(random_matrix, matrix_next)
 
 # plot the converged array
-x = range(n)
-y = range(m)
+print(differ)
+x = range(m)
+y = range(n)
 X, Y = np.meshgrid(x, y)
+
+
 ax = plt.axes(projection='3d')
-
+#ax.scatter(X, Y, differ)
 ax.plot_surface(X, Y, differ)
+ax.plot_surface(X, Y, matrix_prev)
 
+
+ax.view_init(60,35)
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
