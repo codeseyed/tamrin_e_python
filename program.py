ce3af30f28ac61ce3ab0d8281d7e9ad405968d17
masoud gholami <gongotar@masouds-MacBook-Pro.local>
2020-04-13 12:08:14 +0200

    init matrix

--- program.py
+++ program.py
@@ -6,15 +6,13 @@ Created on Tue Mar 31 19:11:54 2020
 @author: gongotar
 """
 import numpy as np
-import random as rnd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-#rnd.seed(1)
 
-def randomarray(n,m):
+def generatearray(n,m):
     h= 1
-    vector = np.arange(m*n, step=h, dtype=np.int)
+    vector = np.arange(m*n, step=h, dtype=np.float)
     random_matrix = np.reshape(vector, (n,m))
     return random_matrix
 
@@ -47,27 +45,27 @@ def neigh_average(random_matrix):
                 
     return averaged_matrix
 
-def dffer(random_matrix, averaged_matrix):
-    difference = np.zeros(random_matrix.shape)
-    for i in range(len(random_matrix)):
-        for j in range(len(random_matrix[0])):
-            difference[i][j] = abs(random_matrix[i][j] - averaged_matrix[i][j])
+def dffer(init_matrix, averaged_matrix):
+    difference = np.zeros(init_matrix.shape)
+    for i in range(len(init_matrix)):
+        for j in range(len(init_matrix[0])):
+            difference[i][j] = abs(init_matrix[i][j] - averaged_matrix[i][j])
     return difference
 
 
 # generate n * m 2D array with random numbers
 n = 50
 m = 50
-random_matrix = randomarray(n, m)
+init_matrix = generatearray(n, m)
 
 # store it in file
-np.savetxt('random_matrix.csv', random_matrix, delimiter=',', fmt='%d')
+np.savetxt('init_matrix.csv', init_matrix, delimiter=',', fmt='%d')
 
 # compute average of each cell with its neighbours until converge
-matrix_prev = random_matrix
+matrix_prev = init_matrix
 matrix_next = neigh_average(matrix_prev)
 differ = dffer(matrix_prev, matrix_next)
-while sum(sum(differ)) > m*n: #check whether convergance is achieved!
+while sum(sum(differ)) > 1: #check whether convergance is achieved!
     matrix_prev = matrix_next
     matrix_next = neigh_average(matrix_prev)
     differ = dffer(matrix_prev, matrix_next)
@@ -85,6 +83,8 @@ B = np.loadtxt('random_matrix.csv', delimiter=',')
 
 # plot the converged array
 print(differ)
+differ = dffer(init_matrix, matrix_next)
+
 x = range(m)
 y = range(n)
 X, Y = np.meshgrid(x, y)
@@ -93,7 +93,7 @@ X, Y = np.meshgrid(x, y)
 ax = plt.axes(projection='3d')
 #ax.scatter(X, Y, differ)
 ax.plot_surface(X, Y, differ)
-ax.plot_surface(X, Y, matrix_prev)
+#ax.plot_surface(X, Y, init_matrix)
 
 
 ax.view_init(60,35)
