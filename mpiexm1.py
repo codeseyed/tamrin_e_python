315bfd08c6ebcee2fb484e5c5525dd8681d64464
Mojtaba <seyedraoufi@gmail.com>
2020-04-18 14:41:12 +0200

    adding another communication!

--- mpiexm1.py
+++ mpiexm1.py
@@ -12,18 +12,26 @@ import numpy as np
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
+def arraygen(array):
+    return [i*(rank+5) for i in array] 
+
 # generating an array in process 0 then send it to process 1 for modification
 if rank == 0:
     array = np.arange(1, 10, step=1, dtype=np.int)
-    comm.send(array, dest=1, tag=1)
+    array_new = arraygen(array)
+    comm.send(array_new, dest=1, tag=1)
+    array_newer = comm.recv(source=1, tag=2)
+    
     
 elif rank == 1:
-    array = comm.recv(source=0, tag=1)
-    array = [i*(rank+1) for i in array]
+    array_new = comm.recv(source=0, tag=1)
+    array_newer = arraygen(array_new)
+    comm.send(array_newer, dest=0, tag=2)
     
     
-    
-print(rank, array)
+print(array_new, array_newer)  
+#array = [i*(rank+5) for i in array]   
+#print(rank, array, new)
 
         
         
