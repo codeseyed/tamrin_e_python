6bd0577ecc36f9335948727c38cc5d2c2b344ff4
Mojtaba <seyedraoufi@gmail.com>
2020-04-20 21:59:20 +0200

    new!

--- mpiexm1.py
+++ mpiexm1.py
@@ -15,23 +15,30 @@ rank = comm.Get_rank()
 def arraygen(array):
     return [i*(rank+5) for i in array] 
 
-# generating an array in process 0 then send it to process 1 for modification
+# generating an array between 1 to 10
+array = np.arange(1, 10, step=1, dtype=np.int)
+
+# modifying the array based on the rank
+array_new = arraygen(array)
+array_newer = arraygen(array)
+
+# distributign the process
 if rank == 0:
-    array = np.arange(1, 10, step=1, dtype=np.int)
-    array_new = arraygen(array)
-    comm.send(array_new, dest=1, tag=1)
+    comm.send(array, dest=1, tag=1)
+    comm.send(array_new, dest=1, tag=3)
     array_newer = comm.recv(source=1, tag=2)
     
     
+    
 elif rank == 1:
-    array_new = comm.recv(source=0, tag=1)
-    array_newer = arraygen(array_new)
+    array = comm.recv(source=0, tag=1)
+    array_new = comm.recv(source=0, tag=3) 
     comm.send(array_newer, dest=0, tag=2)
-    
-    
-print(array_new, array_newer)  
-#array = [i*(rank+5) for i in array]   
-#print(rank, array, new)
+
+print(rank, array, array_new, array_newer)
+  
+
+
 
         
         
