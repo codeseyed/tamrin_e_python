08a57b3d16fe09351b0267e3cd29b0e1cb52a3b0
Mojtaba <seyedraoufi@gmail.com>
2020-04-23 16:39:37 +0200

    updated!

--- mpiexm2.py
+++ mpiexm2.py
@@ -7,6 +7,7 @@ Created on Wed Apr 22 17:58:52 2020
 """
 
 from mpi4py import MPI
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -14,17 +15,29 @@ size = comm.Get_size()
 
 
 if rank == 0:
-    our_integer = 100
+    our_integer = np.cos(rank)
     comm.send(our_integer, dest=1, tag=1)
-    our_integer = comm.recv(source=size-1, tag=size+1)
+    nest_int = comm.recv(source=1, tag=0)
+    ave_int = (our_integer + nest_int)/2
+    averaged_num =[ave_int]
+    for i in range(1,size):
+        num = comm.recv(source=i, tag=i)
+        averaged_num.append(num)
+    print(averaged_num)
+        
+    
 elif rank < size -1:
-    our_integer = comm.recv(source=rank-1, tag=rank)
-    our_integer += rank
+    our_integer = np.cos(rank)
     comm.send(our_integer, dest=rank+1, tag=rank+1)
+    comm.send(our_integer, dest=rank-1, tag=rank-1)
+    prev_int = comm.recv(source=rank-1, tag=rank)
+    nest_int = comm.recv(source=rank+1, tag=rank)
+    ave_int = (our_integer + prev_int + nest_int)/3
+    comm.send(ave_int, dest=0, tag=rank)
 else:
-    our_integer = comm.recv(source=size-2, tag=size-1)
-    our_integer += rank
-    comm.send(our_integer, dest=0, tag=size+1)
+    our_integer = np.cos(rank)
+    comm.send(our_integer, dest=size-2, tag=rank-1)
+    prev_int = comm.recv(source=size-2, tag=rank)
+    ave_int = (our_integer + prev_int)/2
+    comm.send(ave_int, dest=0, tag=rank)
         
-        
-print(rank, our_integer)
