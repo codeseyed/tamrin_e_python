5f07b2bbb482fa953c5f2d86d79621720da63ee7
Mojtaba <seyedraoufi@gmail.com>
2020-04-24 22:48:31 +0200

    !

--- mpiexm2.py
+++ mpiexm2.py
@@ -14,16 +14,15 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 
+
+
 if rank == 0:
     our_integer = np.cos(rank)
     comm.send(our_integer, dest=1, tag=1)
     nest_int = comm.recv(source=1, tag=0)
     ave_int = (our_integer + nest_int)/2
-    averaged_num =[ave_int]
-    for i in range(1,size):
-        num = comm.recv(source=i, tag=i)
-        averaged_num.append(num)
-    print(averaged_num)
+
+    
         
     
 elif rank < size -1:
@@ -33,11 +32,21 @@ elif rank < size -1:
     prev_int = comm.recv(source=rank-1, tag=rank)
     nest_int = comm.recv(source=rank+1, tag=rank)
     ave_int = (our_integer + prev_int + nest_int)/3
-    comm.send(ave_int, dest=0, tag=rank)
+    
 else:
     our_integer = np.cos(rank)
     comm.send(our_integer, dest=size-2, tag=rank-1)
     prev_int = comm.recv(source=size-2, tag=rank)
     ave_int = (our_integer + prev_int)/2
-    comm.send(ave_int, dest=0, tag=rank)
-        
+    
+average = comm.gather(ave_int, root=0)
+print(rank,average)
+
+if rank == 0:
+    print(average)
+    ave_average = sum(average)/len(average)
+    print(ave_average)
+    
+
+ 
+
