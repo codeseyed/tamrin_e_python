4a84feb231eb479cddab8e7b0365cf534f802729
Mojtaba <seyedraoufi@gmail.com>
2020-04-26 18:24:37 +0200

    reduced for array!

--- mpiexm2.py
+++ mpiexm2.py
@@ -9,15 +9,15 @@ Created on Wed Apr 22 17:58:52 2020
 from mpi4py import MPI
 import numpy as np
 
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
 
-
+our_integer = np.full(10,rank, dtype=int)
 
 if rank == 0:
-    our_integer = np.cos(rank)
     comm.send(our_integer, dest=1, tag=1)
     nest_int = comm.recv(source=1, tag=0)
     ave_int = (our_integer + nest_int)/2
@@ -26,7 +26,6 @@ if rank == 0:
         
     
 elif rank < size -1:
-    our_integer = np.cos(rank)
     comm.send(our_integer, dest=rank+1, tag=rank+1)
     comm.send(our_integer, dest=rank-1, tag=rank-1)
     prev_int = comm.recv(source=rank-1, tag=rank)
@@ -34,11 +33,11 @@ elif rank < size -1:
     ave_int = (our_integer + prev_int + nest_int)/3
     
 else:
-    our_integer = np.cos(rank)
     comm.send(our_integer, dest=size-2, tag=rank-1)
     prev_int = comm.recv(source=size-2, tag=rank)
     ave_int = (our_integer + prev_int)/2
-    
+
+print(rank, ave_int)    
 average = comm.reduce(ave_int, op = MPI.SUM, root=0)
 #average = comm.gather(ave_int, root=0)
 
