685b4f0bab48e8722569a34703ac15655abf9b44
Mojtaba <seyedraoufi@gmail.com>
2020-04-27 19:38:40 +0200

    it is not working!

--- mpiexm2.py
+++ mpiexm2.py
@@ -15,34 +15,38 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 
-our_integer = np.full(10,rank, dtype=int)
+if rank == 0:
+    group_of_arr = []
+    for i in range(size):
+        samp_arr = np.full(10, i, dtype=int)
+        group_of_arr.append(samp_arr)
+
+# distributing data to all ranks
+our_array = comm.scatter(group_of_arr, root=0)
 
 if rank == 0:
-    comm.send(our_integer, dest=1, tag=1)
-    nest_int = comm.recv(source=1, tag=0)
-    ave_int = (our_integer + nest_int)/2
-
-    
-        
+    comm.send(our_array, dest=1, tag=1)
+    nest_arr = comm.recv(source=1, tag=0)
+    ave_arr = (our_array + nest_arr)/2
+       
     
 elif rank < size -1:
-    comm.send(our_integer, dest=rank+1, tag=rank+1)
-    comm.send(our_integer, dest=rank-1, tag=rank-1)
-    prev_int = comm.recv(source=rank-1, tag=rank)
-    nest_int = comm.recv(source=rank+1, tag=rank)
-    ave_int = (our_integer + prev_int + nest_int)/3
+    comm.send(our_array, dest=rank+1, tag=rank+1)
+    comm.send(our_array, dest=rank-1, tag=rank-1)
+    prev_arr = comm.recv(source=rank-1, tag=rank)
+    nest_arr = comm.recv(source=rank+1, tag=rank)
+    ave_arr = (our_array + prev_arr + nest_arr)/3
     
 else:
-    comm.send(our_integer, dest=size-2, tag=rank-1)
-    prev_int = comm.recv(source=size-2, tag=rank)
-    ave_int = (our_integer + prev_int)/2
+    comm.send(our_array, dest=size-2, tag=rank-1)
+    prev_arr = comm.recv(source=size-2, tag=rank)
+    ave_arr = (our_array + prev_arr)/2
 
-print(rank, ave_int)    
-average = comm.reduce(ave_int, op = MPI.SUM, root=0)
-#average = comm.gather(ave_int, root=0)
+print(rank, ave_arr)    
+#average = comm.allgather(ave_arr)
+#average = comm.gather(ave_arr, root=0)
 
-if rank == 0:
-    print(average/size)
+#print(average)
     #ave_average = sum(average)/len(average)
     #print(ave_average)
     
